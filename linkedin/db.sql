a3afd594474ab8280392b5b549d239e7109e935c
Amit Hazra <shivahazra@gmail.com>
2018-07-03 23:24:26 +0530

    File Changed

--- linkedin/db.sql
+++
@@ -1,8 +0,0 @@
-CREATE DATABASE IF NOT EXISTS login_system;
-USE login_system;
-CREATE TABLE IF NOT EXISTS `users` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
